757fe6a0fa6dd36a7db582c0daefab12dc1ead2e
Christian Brandt <christianbrandt90@gmail.com>
2013-05-30 13:37:49 +0200

    fixes issue #13

--- Chart.js
+++ Chart.js
@@ -1651,16 +1651,6 @@ window.Chart = function(context, options){
 			}
 		}
 	}
-
-    //Populate an array of all the labels by interpolating the string.
-    function populateLabels(labelTemplateString, labels, numberOfSteps, graphMin, stepValue) {
-        if (labelTemplateString) {
-            //Fix floating point errors by setting to fixed the on the same decimal as the stepValue.
-            for (var i = 1; i < numberOfSteps + 1; i++) {
-                labels.push(tmpl(labelTemplateString, {value: (graphMin + (stepValue * i)).toFixed(getDecimalPlaces(stepValue))}));
-            }
-        }
-    }
 	
 	//Max value from array
 	function Max( array ){
